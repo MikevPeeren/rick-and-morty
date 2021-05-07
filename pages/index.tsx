43f4c35a836c0db438ae9f1d89ab4e050053f347
MikevPeeren <MikevPeeren@hotmail.com>
2021-05-07 13:19:53 +0200

    :sparkles: added new image and gather character information form

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,43 @@
-import Head from 'next/head'
-import Image from 'next/image'
+// NextJS
+import Head from 'next/head';
+import Image from 'next/image';
 
+// React
+import React, { ReactElement } from 'react';
+
+// Components
+import CharacterFetcher from './CharacterFetcher';
 export default function Home() {
   return (
-    <div >
+    <div>
       <Head>
         <title>Rick and Morty Characters</title>
         <meta name="description" content="Rick and Morty Characters" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main >
-        
+      <main>
+        <div className="m-10 flex flex-col">
+          <div className="text-center">
+            <Image
+              className="w-full rounded-2xl"
+              src="/background.jpg"
+              alt="Rick Enjoying Flower Garden"
+              width={350}
+              height={250}
+            />
+          </div>
+        </div>
+        <div>
+          <CharacterFetcher />
+        </div>
       </main>
 
       <footer>
-        <a
-          href="https://mikevpeeren.nl"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        {/* <a href="https://mikevpeeren.nl" target="_blank" rel="noopener noreferrer">
           Powered by Mike van Peeren
-        </a>
+        </a> */}
       </footer>
     </div>
-  )
+  );
 }
