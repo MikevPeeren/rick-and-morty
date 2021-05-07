bac4dc147ae0ddfe3a30c52ab20d622107b736f9
MikevPeeren <MikevPeeren@hotmail.com>
2021-05-07 12:01:22 +0200

    :heavy_plus_sign: adding tailwind/sass

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: ['./pages/**/*.{js,ts,tsx}', './components/**/*.{js,ts,tsx}'],
+  theme: {},
+  variants: {},
+  plugins: [],
+  darkMode: 'class',
+};
