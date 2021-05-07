80e73d0d3522f823a2d237b4b882928c9dcf14c8
MikevPeeren <MikevPeeren@hotmail.com>
2021-05-07 11:38:07 +0200

    :sparkles: initialization nextjs

---
+++ pages/index.tsx
@@ -0,0 +1,28 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div >
+      <Head>
+        <title>Rick and Morty Characters</title>
+        <meta name="description" content="Rick and Morty Characters" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main >
+        
+      </main>
+
+      <footer>
+        <a
+          href="https://mikevpeeren.nl"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by Mike van Peeren
+        </a>
+      </footer>
+    </div>
+  )
+}
